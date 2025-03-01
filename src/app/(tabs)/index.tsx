c5e68d03459f8226d20910b686c053e626cbeb3b
jcpmbhc <jpineda@banhcafe.hn>
2025-02-28 18:36:49 -0600

    firsst commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, View } from "react-native";
+
+import { ThemedText } from "@/components/ThemedText";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ThemedText type="title">Home Screen</ThemedText>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
