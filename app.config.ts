f8f8bc17d7b08015e80a769671b1edf587204aca
Jeral Pineda <jeral.cris@gmail.com>
2025-03-07 21:33:28 -0600

    fix: configure Preview Builds and environments for App Variant

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,41 @@
 const environment = process.env.NODE_ENV;
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_QA = process.env.APP_VARIANT === "preview";
+
+interface EnvironmentValues {
+  name: string;
+  bundleIdentifier: string;
+  package: string;
+}
+
+function getEnvironmetValues(): EnvironmentValues {
+  if (IS_DEV) {
+    return {
+      name: "Course App (DEV)",
+      bundleIdentifier: "com.jeral495.rncourseapp.dev",
+      package: "com.jeral495.rncourseapp.dev",
+    };
+  }
+
+  if (IS_QA) {
+    return {
+      name: "Course App (QA)",
+      bundleIdentifier: "com.jeral495.rncourseapp.qa",
+      package: "com.jeral495.rncourseapp.qa",
+    };
+  }
+
+  return {
+    name: "Course App JP",
+    bundleIdentifier: "com.jeral495.rncourseapp",
+    package: "com.jeral495.rncourseapp",
+  };
+}
 
 const myUrl = environment === "development" ? "http://localhost:8081" : "https://jeralpineda.dev";
 
 export default {
-  name: "rn-course-app",
+  name: getEnvironmetValues().name,
   slug: "rn-course-app",
   owner: "jeral495",
   version: "1.0.0",
@@ -14,13 +46,13 @@ export default {
   newArchEnabled: true,
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "com.jeral495.rncourseapp",
+    bundleIdentifier: getEnvironmetValues().bundleIdentifier,
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
     },
   },
   android: {
-    package: "com.jeral495.rncourseapp",
+    package: getEnvironmetValues().package,
     adaptiveIcon: {
       foregroundImage: "./src/assets/images/adaptive-icon.png",
       backgroundColor: "#ffffff",
@@ -55,5 +87,11 @@ export default {
     eas: {
       projectId: "cfd43cbc-a273-4e15-9aad-38693193bace",
     },
+  },
+  updates: {
+    url: "https://u.expo.dev/cfd43cbc-a273-4e15-9aad-38693193bace",
+  },
+  runtimeVersion: {
+    policy: "appVersion",
   },
 };
